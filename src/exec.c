955fc819aafa74583c63a9ccc4961be532e5566e
Kevin Bowling <kbowling@llnw.com>
2014-04-21 21:21:06 -0700

    [METRICS-380] Allow exec module to exec as root

--- src/exec.c
+++ src/exec.c
@@ -420,11 +420,6 @@ static int fork_child (program_list_t *pl, int *fd_in, int *fd_out, int *fd_err)
 
   uid = sp.pw_uid;
   gid = sp.pw_gid;
-  if (uid == 0)
-  {
-    ERROR ("exec plugin: Cowardly refusing to exec program as root.");
-    return (-1);
-  }
 
   /* The group configured in the configfile is set as effective group, because
    * this way the forked process can (re-)gain the user's primary group. */
